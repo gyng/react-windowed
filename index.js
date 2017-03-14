74c896087e5ebcaeaf23641fc5e5a6dcd298a74d
Ng Guoyou <ng.guoyou@gmail.com>
2017-03-14 15:28:49 +0800

    Add packaging files

---
+++ index.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+
+var Group = require('./lib/components/Group');
+var Widget = require('./lib/components/Widget');
+
+module.exports = {
+  Group: Group,
+  Widget: Widget,
+};
